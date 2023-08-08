24d761293a96a15fc2bf589901d258a1ce0df585
ying <ying285@hotmail.com>
2023-08-08 22:30:34 +0200

    url4

--- src/app/utility/data.ts
+++ src/app/utility/data.ts
@@ -1,6 +1,6 @@
 
 export async function getChartsData(){
-    const res= await fetch('http://localhost:3000//api/charts', {
+    const res= await fetch('http://localhost:3000/api/charts', {
         cache:'no-store'
     });
     return res.json()

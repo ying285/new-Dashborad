93df09153496c3946af85433c71731c9e35c2717
ying <ying285@hotmail.com>
2023-08-09 18:13:48 +0200

    config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first")
 const nextConfig = {
+  
     images: {
         remotePatterns: [
           {
